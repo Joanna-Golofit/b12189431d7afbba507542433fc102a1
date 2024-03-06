2f233bfc71f7e28ece4193a95c30c32849e6af0b
Joanna-Golofit <sunderlajsa@gmail.com>
2024-03-06 18:53:20 +0100

    added setTimeout, not fetching before clicking the button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 interface CommentsData {
@@ -12,27 +12,39 @@ interface CommentsData {
 function App() {
   const [randomCommentId, setRandomCommentId] = useState(1);
   const [data, setData] = useState<CommentsData | null>(null);
+  const [isPending, setIsPending] = useState(false);
   const apiUrl = "https://jsonplaceholder.typicode.com/comments";
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`${apiUrl}/${randomCommentId}`);
-        const userData = await response.json();
+  // useEffect(() => {
+  const fetchData = async () => {
+    try {
+      setIsPending(true);
+      const response = await fetch(`${apiUrl}/${randomCommentId}`);
+      const userData = await response.json();
+
+      setTimeout(() => {
         console.log("Random Data:", userData);
         setData(userData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+        setIsPending(false);
+      }, 2000);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setIsPending(false);
+      // }
+    } finally {
+      // setIsPending(false);
+      setRandomCommentId((prev) => prev + 1);
+    }
+  };
 
-    fetchData();
-  }, [randomCommentId]);
+  // fetchData();
+  // }, [randomCommentId]);
 
   return (
     <>
-      <button onClick={() => setRandomCommentId((prev) => prev + 1)}>
-        Fetch Data {randomCommentId}
+      {/* <button onClick={() => setRandomCommentId((prev) => prev + 1)}> */}
+      <button onClick={fetchData}>
+        {isPending ? "pending" : `Fetch Data ${randomCommentId}`}
       </button>
       <p>
         <b>{data?.name}</b>
